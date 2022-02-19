a71951b9d6ac1ee34abbef1a8281f911009450f1
nghia <nghiangungoc>
2022-02-19 14:36:54 +0700

    first commit

---
+++ src/route/web.js
@@ -0,0 +1,15 @@
+import express from "express";
+import homeController from "../controllers/homeController";
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+    router.get('/', homeController.getHomePage);
+
+    router.get('/hoidanit', (req, res) => {
+        return res.send('Hello world with hoidanit')
+    });
+
+    return app.use("/", router);
+}
+
+module.exports = initWebRoutes;
